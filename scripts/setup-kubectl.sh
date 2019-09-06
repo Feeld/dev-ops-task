2f23d2724ee4d8ea53ec14d9820b3fa837c28b7f
Dave Williams <dave@dave.io>
2019-09-07 00:36:23 +0100

    api-daemon deployed and working, secrets integration pending

---
+++ scripts/setup-kubectl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcloud beta container clusters get-credentials primary --region europe-west2 --project feeld-daveio
--- terraform/resource.google_container_cluster.primary.tf
+++ terraform/resource.google_container_cluster.primary.tf
@@ -28,3 +28,7 @@ output "client_key" {
 output "cluster_ca_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
+
+output "kubernetes_endpoint" {
+  value = "${google_container_cluster.primary.endpoint}"
+}
