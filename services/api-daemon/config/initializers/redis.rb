9c307c4c89759425d127e2d73c02a54d2b7d2ae0
Dave Williams <dave@dave.io>
2019-09-06 17:11:06 +0100

    checkpoint

--- services/api-daemon/config/initializers/redis.rb
+++ services/api-daemon/config/initializers/redis.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Resque.redis = Redis.new(
-  url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' },
-  password: ENV.fetch('REDIS_PASSWORD') { '' }
+  url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' }
 )
 
